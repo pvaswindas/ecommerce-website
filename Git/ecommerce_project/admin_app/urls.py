06873a90f96cad34c838b046b65ef104170266f1
[Aswindas-pv] <[aswinpv1801@gmail.com]>
2024-03-14 15:19:59 +0530

    [fix: brand validation]

--- Git/ecommerce_project/admin_app/urls.py
+++ Git/ecommerce_project/admin_app/urls.py
@@ -2,14 +2,29 @@ from django.urls import path
 from admin_app import views
 
 urlpatterns = [
-    path('sneakerheads/admin/login/', views.admin_login, name='admin_login'),
+    path('sneakerheads/admin/login/', views.admin_login_page, name='admin_login_page'),
     path('sneakerheads/admin/logout/', views.admin_logout, name = 'admin_logout'),
     
-    path('sneakerheads/admin/products/', views.list_product, name='list_product'),
+    
+    path('page_not_found/', views.page_not_found, name='page_not_found'),
+    
+    
+    path('sneakerheads/admin/products/', views.list_product_page, name='list_product_page'),
     path('sneakerheads/admin/add-products/', views.admin_add_product, name = 'admin_add_product'),
-    path('sneakerheads/admin/brands/', views.list_brand, name='list_brand'),
+    path('add-product/page/', views.add_products, name='add_products'),
+    
+    path('sneakerheads/admin/brands/', views.list_brand_page, name='list_brand_page'),
     path('sneakerheads/admin/add-brand/', views.admin_add_brand, name='admin_add_brand'),
-    path('add-brand/page', views.add_brand, name='add_brand'),
+    path('add-brand/page/', views.add_brand, name='add_brand'),
+    path('list-brand/<int:brand_id>/', views.list_the_brand, name='list_the_brand'),
+    path('un-list-brand/<int:brand_id>/', views.un_list_the_brand, name='un_list_the_brand'),
+    path('delete-brand/<int:brand_id>/', views.delete_brand, name='delete_brand'),
+    path('deleted-brand-view', views.deleted_brand_view, name='deleted_brand_view'),
+    path('restore-brand/<int:brand_id>/', views.restore_brand, name='restore_brand'),
+    path('edit-brand/<int:brand_id>/', views.edit_brand, name='edit_brand'),
+    
+    
+    
     
     
     path('sneakerheads/admin/categories/', views.admin_categories, name = 'admin_categories'),
@@ -18,14 +33,13 @@ urlpatterns = [
     path('delete-category/<int:cat_id>/', views.delete_category, name='delete_category'),
     path('list-category/<int:cat_id>/', views.list_category, name='list_category'),
     path('un-list-category/<int:cat_id>/', views.un_list_category, name='un_list_category'),
-    path('deleted-view', views.deleted_cat_view, name='deleted_cat_view'),
+    path('deleted-category-view', views.deleted_cat_view, name='deleted_cat_view'),
     path('restore-categories/<int:cat_id>/', views.restore_categories, name='restore_categories'),
+    
     
     
     path('sneakerheads/admin/customers/', views.admin_customers, name = 'admin_customers'),
     path('block-user/<int:user_id>/', views.block_user, name='block_user'),
     path('unblock-user/<int:user_id>/', views.unblock_user, name='unblock_user'),
     path('sneakerheads/admin/customer/search/', views.search_user, name='search_user'),
-    
-    
 ]
--- Git/ecommerce_project/admin_app/views.py
+++ Git/ecommerce_project/admin_app/views.py
@@ -15,7 +15,7 @@ from django.views.decorators.cache import never_cache
 
 
 @never_cache
-def admin_login(request):
+def admin_login_page(request):
     if request.user.is_authenticated and request.user.is_superuser:
             user_list = User.objects.all().order_by('username').values()
             return render(request, 'admin_index.html', {'user_list': user_list})
@@ -30,15 +30,45 @@ def admin_login(request):
             return render(request, 'admin_index.html', {'user_list': user_list})
         else:
             messages.error(request, 'Invalid credentials, please try logging in again.')
-            return redirect('admin_login')
+            return redirect('admin_login_page')
+ 
+    return render(request, 'admin_login.html')
+
+
+
+
+
+
+@login_required
+@never_cache     
+def admin_logout(request):
+    if request.method == 'POST' :
+        auth.logout(request)
+        messages.info(request, 'Login again!')
+        return redirect('admin_login_page')
     else:
-        return render(request, 'admin_login.html')
+        return redirect('admin_login_page')
+
+ 
+
+
+
+# PAGE NOT FOUND   
+@login_required
+@never_cache
+def page_not_found(request):
+    if request.user.is_superuser:
+        return redirect('admin_login_page')
+    else:
+        return render(request, 'pages/samples/error-404.html')
 
 
 
 
 
 
+
+# CUSTOMER
 @login_required
 @never_cache
 def admin_customers(request):
@@ -47,7 +77,8 @@ def admin_customers(request):
         customer_list = Customer.objects.all().order_by('user__first_name').values()
         return render(request, 'pages/customers/customers.html',{'user_list' : user_list, 'customer_list' : customer_list})
     else:
-        return redirect('admin_login')
+        return redirect('admin_login_page')
+        
         
         
 @login_required
@@ -62,7 +93,7 @@ def block_user(request, user_id):
         else:
             return redirect(admin_customers)
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
 
 
@@ -78,7 +109,7 @@ def unblock_user(request, user_id):
         else:
             return redirect(admin_customers)
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
         
         
         
@@ -97,19 +128,8 @@ def search_user(request):
         return render(request,'pages/customers/customers.html' ,{'user_list' : user_list})
     
     
-
-        
-@login_required
-@never_cache     
-def admin_logout(request):
-    if request.method == 'POST' :
-        auth.logout(request)
-        messages.info(request, 'Login again!')
-        return redirect('admin_login')
-    else:
-        return redirect(admin_login)
-
- 
+    
+    
     
     
     
@@ -121,7 +141,7 @@ def admin_categories(request):
         category_list = Category.objects.filter(is_deleted = False).order_by('name').values()
         return render(request, 'pages/category/category.html', {'category_list' : category_list})
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
     
     
@@ -131,7 +151,7 @@ def add_category_page(request):
     if request.user.is_superuser:
         return render(request, 'pages/category/add_category_page.html')
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
     
     
@@ -154,7 +174,7 @@ def add_categories(request):
                 messages.error(request, 'Category already exists, create new category')
                 return redirect(add_category_page)
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
     
     
@@ -173,7 +193,7 @@ def delete_category(request, cat_id):
             messages.error(request, 'id cannot be found!')
             return redirect(admin_categories)
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
     
     
@@ -192,7 +212,7 @@ def list_category(request, cat_id):
             messages.error(request, 'id cannot be found!')
             return redirect(admin_categories)
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
 
 
 
@@ -212,7 +232,7 @@ def un_list_category(request, cat_id):
             messages.error(request, 'id cannot be found!')
             return redirect(admin_categories)
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
     
 
@@ -223,7 +243,7 @@ def deleted_cat_view(request):
         category_list = Category.objects.filter(is_deleted = True).order_by('name').values()
         return render(request, 'pages/category/deleted_categories.html', {'category_list' : category_list})
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
     
 
@@ -242,7 +262,7 @@ def restore_categories(request, cat_id):
             messages.error(request, ' id cannot be found!')
             return redirect(deleted_cat_view)
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
     
     
@@ -251,22 +271,26 @@ def restore_categories(request, cat_id):
 # PRODUCT
 @login_required
 @never_cache
-def list_product(request):
+def list_product_page(request):
     if request.user.is_superuser:
         product_list = Product.objects.filter(is_deleted = False).order_by('name').values()
         return render(request, 'pages/products/product.html', {'product_list' : product_list})
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
 
 
+
+@login_required
+@never_cache
 def admin_add_product(request):
     if request.user.is_superuser:
         return render(request, 'pages/products/add_products.html')
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
 
 
-# PRODUCTS
+
+
 @login_required
 @never_cache
 def add_products(request):
@@ -297,10 +321,10 @@ def add_products(request):
                     back_view_image = back_view_image,
                     )
                 messages.success(request, 'New Product was created!')
-                return redirect(list_product)
+                return redirect(list_product_page)
         
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
     
     
     
@@ -311,12 +335,12 @@ def add_products(request):
 # BRAND
 @login_required
 @never_cache
-def list_brand(request):
+def list_brand_page(request):
     if request.user.is_superuser:
         brand_list = Brand.objects.filter(is_deleted = False).order_by('name').values()
         return render(request, 'pages/products/brand.html', {'brand_list' : brand_list})
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
 
 
 @login_required
@@ -325,7 +349,7 @@ def admin_add_brand(request):
     if request.user.is_superuser:
         return render(request, 'pages/products/add_brand.html')
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
 
 
 @login_required
@@ -347,9 +371,108 @@ def add_brand(request):
                 )
                 brand.save()
                 messages.success(request, 'New brand added!')
-                return redirect(list_brand)
+                return redirect(list_brand_page)
             else:
                 messages.error(request, 'Brand already exits, add new brand')
                 return redirect(admin_add_brand)
     else:
-        return redirect(admin_login)
+        return redirect('admin_login_page')
+    
+    
+ 
+@login_required
+@never_cache
+def edit_brand(request, brand_id):
+    if request.user.is_superuser:
+        brand_list = Brand.objects.filter(id = brand_id).values()
+        return render(request, 'pages/products/edit_brand.html', {'brand_list' : brand_list})
+    else:
+        return redirect('admin_login_page')
+    
+
+@login_required
+@never_cache
+def delete_brand(request, brand_id):
+    if request.user.is_superuser:
+        if brand_id:
+            brand_to_delete = Brand.objects.get(id = brand_id)
+            brand_to_delete.is_deleted = True
+            brand_to_delete.save()
+            messages.success(request, 'Brand has been deleted!')
+            return redirect(list_brand_page)
+        else:
+            messages.error(request, 'id cannot be found!')
+            return redirect(list_brand_page)
+    else:
+        return redirect('admin_login_page')
+    
+
+
+
+@login_required
+@never_cache
+def restore_brand(request, brand_id):
+    if request.user.is_superuser:
+        if brand_id:
+            brand_to_restore = Brand.objects.get(id = brand_id)
+            brand_to_restore.is_deleted = False
+            brand_to_restore.save()
+            messages.success(request, 'Brand has been restored!')
+            return redirect(list_brand_page)
+        else:
+            messages.error(request, 'id cannot be found!')
+            return redirect(list_brand_page)
+    else:
+        return redirect('admin_login_page')
+    
+    
+
+
+@login_required
+@never_cache
+def deleted_brand_view(request):
+    if request.user.is_superuser:
+        brand_list = Brand.objects.filter(is_deleted = True).order_by('name').values()
+        return render(request, 'pages/products/deleted_brand.html', {'brand_list' : brand_list})
+    else:
+        return redirect('admin_login_page')
+    
+    
+
+    
+
+@login_required
+@never_cache
+def list_the_brand(request, brand_id):
+    if request.user.is_superuser:
+        if brand_id:
+            brand_to_list = Brand.objects.get(id = brand_id)
+            brand_to_list.is_listed = True
+            brand_to_list.save()
+            messages.success(request, 'Brand updated successfully!')
+            return redirect(list_brand_page)
+        else:
+            messages.error(request, 'id cannot be found!')
+            return redirect(list_brand_page)
+    else:
+        return redirect('admin_login_page')
+    
+    
+    
+    
+    
+@login_required
+@never_cache
+def un_list_the_brand(request, brand_id):
+    if request.user.is_superuser:
+        if brand_id:
+            brand_to_un_list = Brand.objects.get(id = brand_id)
+            brand_to_un_list.is_listed = False
+            brand_to_un_list.save()
+            messages.success(request, 'Brand updated successfully!')
+            return redirect(list_brand_page)
+        else:
+            messages.error(request, 'id cannot be found!')
+            return redirect(list_brand_page)
+    else:
+        return redirect('admin_login_page')
